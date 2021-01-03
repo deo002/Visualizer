d7e4d501eb4fee6848a9493c890d2307f9477445
Dearsh Oberoi <oberoidearsh@gmail.com>
2021-01-03 16:41:55 +0530

    bubble sort animation added

--- public/assets/sort/BubbleSort.js
+++ public/assets/sort/BubbleSort.js
@@ -1,127 +1,147 @@
 const canvas = document.querySelector('canvas');
-const WIDTH = 40, STARTX = 500, STARTY = 600, thickness = 1;
+const c = canvas.getContext('2d');
+
+let finalDestinationFirstBar = null, finalDestinationSecondBar = null;
+let firstBar = null, secondBar = null, timeAtWhichAnimationStarts = 0;
+
+const WIDTH = 40, STARTX = 500, STARTY = 600;
+const colorDuringSwappingAndSearching = '#9f5f80', initialColor = '#ff8e71', finalColor = '#ffba93';
+
+const animationArray = [];
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const c = canvas.getContext('2d');
-
+// event listener to resize the canvas as soon as window is resized
 window.addEventListener('resize', (event) => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
 });
 
+// class defined to construct bars representing numbers
 class Bar {
-    constructor(height, x, y) {
+    constructor(height, x, y, color) {
         this.height = height;
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 
+    // draws the bar, text and shadow effects
     draw() {
-        c.fillStyle = 'pink';
-        c.fillRect(this.x, this.y, WIDTH, this.height); 
-        
+        c.fillStyle = this.color;
+        c.fillRect(this.x, this.y, WIDTH, this.height);
+
         c.fillStyle = "black";
-        
-        c.fillText((-this.height)/10, this.x + 11 , this.y - 10 ,);
+        if ((-this.height) / 10 <= 2) {
+            c.fillText((-this.height) / 10, this.x + 11, this.y + this.height - 10);
+        }
+        else {
+            c.fillText((-this.height) / 10, this.x + 11, this.y - 10);
+        }
         c.font = '20px Calibri';
-        
+
+        c.shadowBlur = 10;
+        c.shadowColor = "black";
+
     }
 
-    drawBorder()
-    {
-        c.fillStyle='#000';
-        c.fillRect(this.x - (thickness) - 2 , this.y - (thickness) + 3, WIDTH + (thickness * 2) + 4, this.height + (thickness * 2) - 7);
+    //draws border on the bars that are being swapped
+    drawBorder() {
+        c.lineWidth = 2;
+        c.strokeStyle = '#000';
+        c.strokeRect(this.x, this.y, WIDTH, this.height);
     }
 }
 
-let finalDestinationI = null, finalDestinationIndex = null, iI = null, iIndex = null, time = 0;
-const animationArray = [];
-
+// animation of swapping bars is produced by this function
 function animate() {
     let request = requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (animationArray[iI].x < finalDestinationI || animationArray[iIndex].x > finalDestinationIndex) {
-        if (animationArray[iI].x < finalDestinationI) {
-            animationArray[iI].x += 1;
+    // changing x-coordinates of first bar until it reaches the position occupied by second bar
+    // and vice-versa for second bar
+    if (animationArray[firstBar].x < finalDestinationFirstBar || animationArray[secondBar].x > finalDestinationSecondBar) {
+        if (animationArray[firstBar].x < finalDestinationFirstBar) {
+            animationArray[firstBar].x += 1;
         }
-        if (animationArray[iIndex].x > finalDestinationIndex) {
-            animationArray[iIndex].x -= 1;
+        if (animationArray[secondBar].x > finalDestinationSecondBar) {
+            animationArray[secondBar].x -= 1;
         }
-        animationArray[iI].drawBorder();
-        animationArray[iIndex].drawBorder();
     }
     else {
         cancelAnimationFrame(request);
     }
-    //console.log(animationArray[iI].x, finalDestinationI);
-    for (let k = 0; k < animationArray.length; ++k)
-        animationArray[k].draw();
-}
 
-function delay(i, index) {
-    setTimeout(async function animateInLoop() {
-        finalDestinationI = animationArray[index].x;
-        finalDestinationIndex = animationArray[i].x;
-        iI = i;
-        iIndex = index;
-        //console.log(iI, iIndex, finalDestinationI, finalDestinationIndex);
-        animate();
-        setTimeout(() => {
-            //console.log(animationArray);
-            const temp = animationArray[i];
-            animationArray[i] = animationArray[index];
-            animationArray[index] = temp;
-            //console.log(animationArray);
-        }, Math.abs(iI - iIndex) * 1000);
-    }, time, i, index);
-}
-
-const BubbleSort = async (inputArray) => {
-
-    for (let i = 0; i < inputArray.length; ++i) {
-        animationArray[i] = new Bar(-inputArray[i] * 10, STARTX + 50 * i, STARTY);
-        //console.log(animationArray[i]);
-        animationArray[i].draw();
-    }
-    //console.log(animationArray);
-    // for (let i = 0; i < inputArray.length; ++i) {
-
-    //     let index = i;
-
-    //     for (let j = i + 1; j < inputArray.length; ++j) {
-    //         if (inputArray[j] < inputArray[index])
-    //             index = j;
-    //     }
-
-    //     if (i === index || i >= inputArray.length|| index >= inputArray.length)
-    //         continue;
-
-    //     delay(i, index);
-    //     time += (Math.abs(i - index) * 1000 + 500);
-    //     const temp = inputArray[i];
-    //     inputArray[i] = inputArray[index];
-    //     inputArray[index] = temp;
-
-    // }
-
-    for (let i = 0; i < inputArray.length; ++i) {
-        for (let j = 1; j < inputArray.length - i; ++j) {
-            if(inputArray[j-1] > inputArray[j])
-            {  
-                delay(j-1, j);
-                time += (Math.abs(1) * 1000 + 500);
-                const temp = inputArray[j-1];
-                inputArray[j-1] = inputArray[j];
-                inputArray[j] = temp;
-            }
+    for (let i = 0; i < animationArray.length; ++i) {
+        if (i !== firstBar && i !== secondBar) {
+            animationArray[i].draw();
         }
     }
 
-    return inputArray;
+    // drawing the two bars being swapped after the others so that they appear over them
+    animationArray[firstBar].draw();
+    animationArray[firstBar].drawBorder();
+    animationArray[secondBar].draw();
+    animationArray[secondBar].drawBorder();
 }
 
-console.log(BubbleSort([4,2,1,3,5]));
+//executing animations after a time delay so that they do not overlap
+
+// animation of swapping bars
+function delaySwappingAnimation(i, index, lastIndex) {
+
+    setTimeout(function animateInLoop() {
+
+        firstBar = i;
+        secondBar = index;
+        finalDestinationFirstBar = animationArray[secondBar].x;
+        finalDestinationSecondBar = animationArray[firstBar].x;
+        animationArray[firstBar].color = colorDuringSwappingAndSearching;
+        animationArray[secondBar].color = colorDuringSwappingAndSearching;
+        animate();
+
+        // swapping the bars in animationArray after animation is complete.
+        // we swap bars using indices, so if we swap elements of animationArray 
+        // before the animation is complete, we wont get the desired animation
+        setTimeout(() => {
+            [animationArray[firstBar], animationArray[secondBar]] = [animationArray[secondBar], animationArray[firstBar]];
+            animationArray[firstBar].color = initialColor;
+            animationArray[secondBar].color = initialColor;
+        }, 1000);
+
+    }, timeAtWhichAnimationStarts, i, index);
+}
+
+//algorithm for bubble sort
+const BubbleSort = async (inputArray) => {
+
+    for (let i = 0; i < inputArray.length; ++i) {
+        animationArray[i] = new Bar(-inputArray[i] * 10, STARTX + 50 * i, STARTY, initialColor);
+        animationArray[i].draw();
+    }
+
+    for (let i = 0; i < inputArray.length; ++i) {
+
+        for (let j = 1; j < inputArray.length - i; ++j) {
+
+            if (inputArray[j - 1] > inputArray[j]) {
+                delaySwappingAnimation(j - 1, j, inputArray.length - i - 1);
+                timeAtWhichAnimationStarts += 1500;
+                [inputArray[j], inputArray[j - 1]] = [inputArray[j - 1], inputArray[j]];
+            }
+        }
+        setTimeout(() => {
+            c.clearRect(0, 0, canvas.width, canvas.height);
+            for (let k = 0; k < animationArray.length; ++k) {
+                if (k === animationArray.length - 1 - i) {
+                    animationArray[k].color = finalColor;
+                }
+                animationArray[k].draw();
+            }
+        }, timeAtWhichAnimationStarts);
+        timeAtWhichAnimationStarts += 50;
+    }
+}
+
+BubbleSort([4, 2, 7, 56, 33, 28, 33, 2, 5]);
