7e28b97d5a3df1b1313de32f684657106e748b95
Akash Deep <61289472+akash2408@users.noreply.github.com>
2021-01-09 23:51:08 +0530

    Update MergeSort.js

--- public/assets/sort/MergeSort.js
+++ public/assets/sort/MergeSort.js
@@ -1,12 +1,10 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-const WIDTH = 60, STARTX = 500, STARTY = 300, STARTYINTERMEDIATE = 500, YSPEED = 5, DELAY = 2000;
+const WIDTH = 60, STARTX = 500, STARTY = 300, STARTYINTERMEDIATE = 500;
 const colorWhileSwapping = '#f3ca20', initialColor = '#000';
 
-const animationArray = [];
-
-let timeAtWhichAnimationStarts = 0, finalDestinationX = null, xSpeed = null, curBar = null;
+let timeAtWhichAnimationStarts = 0, finalDestinationX = null, xSpeed = null, curBar = null,index=0,isPlaying=false,speed=1,YSPEED = 5;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -17,7 +15,7 @@ window.addEventListener('resize', (event) => {
     canvas.height = window.innerHeight;
 });
 
-const inputArray = [6, 3, 6, 4, 8, 5, 2, 9];
+let inputArray = [] ,animationArray = [],downArrayFirst=[],downArraySecond=[],requestDown=null,requestUp=null;
 
 // class defined to construct boxes representing numbers
 class Box {
@@ -44,15 +42,106 @@ class Box {
     }
 }
 
+// merge helper of mergesort function
+const merge = (leftIndex, midIndex, rightIndex) => {
+
+    const leftSize = midIndex - leftIndex + 1;
+    const rightSize = rightIndex - midIndex;
+
+    const left = inputArray.slice(leftIndex, midIndex + 1);
+    const right = inputArray.slice(midIndex + 1, rightIndex + 1);
+
+    let i = 0, j = 0, k = leftIndex;
+
+    while (i < leftSize && j < rightSize) {
+        if (left[i] <= right[j]) {
+            inputArray[k] = left[i];
+
+            // boxes go down to their designated positions
+            //delayGoingDown(i + leftIndex, k,"first");
+            //console.log("first",i + leftIndex, k);
+            downArrayFirst.push(i + leftIndex);
+            downArraySecond.push(k);
+            ++i;
+            ++k;
+        }
+        else {
+            inputArray[k] = right[j];
+            //delayGoingDown(j + midIndex + 1, k,"second");
+            //console.log("second",j + midIndex + 1, k);
+            downArrayFirst.push(j + midIndex + 1);
+            downArraySecond.push(k);
+            ++j;
+            ++k;
+        }
+    }
+
+    while (i < leftSize) {
+        inputArray[k] = left[i];
+        //delayGoingDown(i + leftIndex, k,"third");
+        downArrayFirst.push(i + leftIndex);
+        downArraySecond.push(k);
+       // console.log("third",i + leftIndex, k);
+        ++i;
+        ++k;
+    }
+
+    while (j < rightSize) {
+        inputArray[k] = right[j];
+        //delayGoingDown(j + midIndex + 1, k,"fourth");
+        downArrayFirst.push(j + midIndex + 1);
+        downArraySecond.push(k);
+        //console.log("fourth",j + midIndex + 1, k);
+        ++j;
+        ++k;
+    }
+
+    // after all the boxes have reached their designated positions move the subarray back to its position
+    //delayGoingUp();
+    downArrayFirst.push("UP");
+    downArraySecond.push("UP");
+}
+
+const mergeSortHelper = (leftIndex, rightIndex) => {
+
+    if (rightIndex > leftIndex) {
+        const midIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+        mergeSortHelper(leftIndex, midIndex);
+        mergeSortHelper(midIndex + 1, rightIndex);
+        merge(leftIndex, midIndex, rightIndex);
+    }
+}
+
+//algorithm for merge sort
+function mergeSort(input) {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    $("#slider").val(speed);
+    inputArray=input;
+    $("#stop").html("Stop")
+    animationArray = [],downArrayFirst=[],downArraySecond=[],isPlaying=false,requestDown=null,requestUp=null,index=0,YSPEED=5;
+    for (let i = 0; i < inputArray.length; ++i) {
+        animationArray[i] = new Box(STARTX + 100 * i, STARTY, initialColor, inputArray[i]);
+        animationArray[i].draw();
+    }
+
+    mergeSortHelper(0, inputArray.length - 1);
+}
 // utility function to sort animationArray according to the x-coordinates
 const sortAnimationArray = () => {
     animationArray.sort((a, b) => a.x - b.x);
 }
 
+$("#slider").change(function(){
+    speed=Number($(this).val());
+    console.log(speed);
+})
+
 // function to produce animation that takes the subarray boxes down to a temporary position
 function animateGoingDown() {
 
-    const request = requestAnimationFrame(animateGoingDown);
+    requestDown = requestAnimationFrame(animateGoingDown);
+    requestUp=null;
 
     c.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -69,7 +158,27 @@ function animateGoingDown() {
         }
     }
     else {
-        cancelAnimationFrame(request);
+          cancelAnimationFrame(requestDown);
+          if(index+1 < downArrayFirst.length)
+                {
+                index=index+1;
+                if(downArrayFirst[index]==downArraySecond[index] && downArraySecond[index]=="UP")
+                {   animateGoingUp();
+                    sortAnimationArray();
+                    index=index+1;
+                }
+                else
+                {  
+                    var cur=downArrayFirst[index], final = downArraySecond[index];
+                    animationArray[cur].color=colorWhileSwapping;
+                    animationArray[final].color=colorWhileSwapping;
+                    finalDestinationX = STARTX + 100 * final;
+                    curBar = cur;
+                    // setting xSpeed in accordance to ySpeed so that final x and y coordinate is reached simultaneously
+                    xSpeed = Math.abs(finalDestinationX - animationArray[curBar].x) / (STARTYINTERMEDIATE - STARTY) * YSPEED;
+                    animateGoingDown();
+                }
+                }
     }
 
     for (bar of animationArray) {
@@ -80,7 +189,8 @@ function animateGoingDown() {
 // this function produces animation of subarray boxes going up
 function animateGoingUp() {
 
-    const request = requestAnimationFrame(animateGoingUp);
+    requestUp = requestAnimationFrame(animateGoingUp);
+    requestDown=null
 
     c.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -95,121 +205,96 @@ function animateGoingUp() {
         bar.draw();
     }
     if (!goesUp)
-        cancelAnimationFrame(request);
-}
-
-//executing animations after a time delay so that they do not overlap
-const delayGoingDown = (cur, final) => {
-    setTimeout(() => {
-        finalDestinationX = STARTX + 100 * final;
-        curBar = cur;
-
-        // setting xSpeed in accordance to ySpeed so that final x and y coordinate is reached simultaneously
-        xSpeed = Math.abs(finalDestinationX - animationArray[curBar].x) / (STARTYINTERMEDIATE - STARTY) * YSPEED;
-
-        animateGoingDown();
-
-    }, timeAtWhichAnimationStarts);
-}
-
-const delayGoingUp = () => {
-    setTimeout(() => {
-        animateGoingUp();
-        setTimeout(sortAnimationArray, DELAY / 2);
-    }, timeAtWhichAnimationStarts);
-}
-
-// merge helper of mergesort function
-const merge = (leftIndex, midIndex, rightIndex) => {
-
-    const leftSize = midIndex - leftIndex + 1;
-    const rightSize = rightIndex - midIndex;
-
-    const left = inputArray.slice(leftIndex, midIndex + 1);
-    const right = inputArray.slice(midIndex + 1, rightIndex + 1);
-
-    // setting the subarray being sorted to yellow
-    setTimeout(() => {
-        for (let i = leftIndex; i <= rightIndex; ++i) {
-            animationArray[i].color = colorWhileSwapping;
+        {   YSPEED=5*speed;
+            for (bar of animationArray) {
+                bar.color=initialColor;
+                bar.draw();
+            }
+            cancelAnimationFrame(requestUp);
+            if(index < downArrayFirst.length){
+                var cur=downArrayFirst[index], final = downArraySecond[index];
+                animationArray[cur].color=colorWhileSwapping;
+                animationArray[final].color=colorWhileSwapping;
+                finalDestinationX = STARTX + 100 * final;
+                curBar = cur;
+                // setting xSpeed in accordance to ySpeed so that final x and y coordinate is reached simultaneously
+                xSpeed = Math.abs(finalDestinationX - animationArray[curBar].x) / (STARTYINTERMEDIATE - STARTY) * YSPEED;
+                animateGoingDown();
+            }
         }
-    }, timeAtWhichAnimationStarts);
-    timeAtWhichAnimationStarts += 50;
-
-    let i = 0, j = 0, k = leftIndex;
-
-    while (i < leftSize && j < rightSize) {
-        if (left[i] <= right[j]) {
-            inputArray[k] = left[i];
-
-            // boxes go down to their designated positions
-            delayGoingDown(i + leftIndex, k);
-
-            timeAtWhichAnimationStarts += DELAY;
-            ++i;
-            ++k;
-        }
-        else {
-            inputArray[k] = right[j];
-            delayGoingDown(j + midIndex + 1, k);
-            timeAtWhichAnimationStarts += DELAY;
-            ++j;
-            ++k;
-        }
-    }
-
-    while (i < leftSize) {
-        inputArray[k] = left[i];
-        delayGoingDown(i + leftIndex, k);
-        timeAtWhichAnimationStarts += DELAY;
-        ++i;
-        ++k;
-    }
-
-    while (j < rightSize) {
-        inputArray[k] = right[j];
-        delayGoingDown(j + midIndex + 1, k);
-        timeAtWhichAnimationStarts += DELAY;
-        ++j;
-        ++k;
-    }
-
-    // after all the boxes have reached their designated positions move the subarray back to its position
-    delayGoingUp();
-    timeAtWhichAnimationStarts += DELAY;
-
-    // change color of subarray boxes back to black
-    setTimeout(() => {
-        for (let i = leftIndex; i <= rightIndex; ++i) {
-            animationArray[i].color = initialColor;
-        }
-        for (bar of animationArray)
-            bar.draw();
-    }, timeAtWhichAnimationStarts);
-    timeAtWhichAnimationStarts += 50;
 }
 
-const mergeSortHelper = (leftIndex, rightIndex) => {
+$('#start').click(function(){
+        if($(this).html()=="Replay")
+            {   $(this).html("Start");
+                if(isPlaying==true){
+                    if(requestDown)
+                        {cancelAnimationFrame(requestDown);}
+                    else
+                        {cancelAnimationFrame(requestUp)}
+                }
+                if($('#input').val())
+                    {mergeSort($('#input').val().split(",").map(Number));}
+                else {mergeSort([6, 3, 6, 4, 8, 5, 2, 9]);}
 
-    if (rightIndex > leftIndex) {
-        const midIndex = Math.floor((leftIndex + rightIndex) / 2);
+            }  
+        else if(isPlaying == false) {
+            $(this).html("Replay");
+                YSPEED=5*speed;
+                isPlaying = true;
+                var cur = downArrayFirst[index], final = downArraySecond[index];
+                animationArray[cur].color=colorWhileSwapping;
+                animationArray[final].color=colorWhileSwapping;
+                finalDestinationX = STARTX + 100 * final;
+                curBar = cur;
 
-        mergeSortHelper(leftIndex, midIndex);
-        mergeSortHelper(midIndex + 1, rightIndex);
-        merge(leftIndex, midIndex, rightIndex);
-    }
-}
+                // setting xSpeed in accordance to ySpeed so that final x and y coordinate is reached simultaneously
+                xSpeed = Math.abs(finalDestinationX - animationArray[curBar].x) / (STARTYINTERMEDIATE - STARTY) * YSPEED;
+                animateGoingDown();
+            // }
+        }       
+})
 
-const mergeSort = () => {
+$('#stop').click(function(){
+        if(isPlaying == true)
+          {
+            if(requestDown)
+                {cancelAnimationFrame(requestDown);}
+            else
+                {cancelAnimationFrame(requestUp);}
 
-    for (let i = 0; i < inputArray.length; ++i) {
-        animationArray[i] = new Box(STARTX + 100 * i, STARTY, initialColor, inputArray[i]);
-        animationArray[i].draw();
-    }
+           $(this).html("Play"); 
+           isPlaying = false;
+           }
+        else
+          { if($(this).html()=="Play"){
+                if(requestDown)
+                    {requestAnimationFrame(animateGoingDown);}
+                else
+                    {requestAnimationFrame(animateGoingUp);}
+               $(this).html("Stop");
+               $(this).css("Stop"); 
+               isPlaying=true;
+            }
+         }
+   })
 
-    mergeSortHelper(0, inputArray.length - 1);
-}
+$('#enter').click(function(){
+        if($('#input').val()){
+            $('#start').html("Start");
+            if(isPlaying==true)
+                {   
+                if(requestDown)
+                    {cancelAnimationFrame(requestDown);}
+                else
+                    {cancelAnimationFrame(requestUp);}
+                }
+            mergeSort($('#input').val().split(",").map(Number));
+        }
+   })
+$('#reset').click(function(){
+    speed=1;
+    $("#slider").val(speed);
+   })
 
-mergeSort();
-
-
+mergeSort([6, 3, 6, 4, 8, 5, 2, 9]);
